ecfe5f66de4ed979b491bcc99af9ee8a9a82212b
Tanzania Coleman <tanzania.coleman@gmail.com>
2019-12-10 12:50:24 -0500

    added express and express router to package json also completed routing for application

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,29 @@
+let express = require('express');
+let burger = require('../models/burger');
+let router = express.Router();
+
+
+router.get("/", function (req, res){
+
+    let s = burger.select();
+    let d = burger.devour();
+
+    connection.query("SELECT * FROM burgers", function (err) {
+        if(err) throw err;
+
+        res.render("index", {burgers: s, devoured: d})
+    })
+});
+
+router.post("/api/burgers", function (req, res){
+    burger.inser(req.body.burger_name);
+    res.status(200).end();
+})
+
+router.put("/api/burgers/:id", function(req,res){
+
+    burger.update(req.params.id);
+    res.status(200).end()
+})
+
+module.exports = router;
