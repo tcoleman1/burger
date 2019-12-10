e9774090abb2d558ed440a9d42502b834fa4cd2a
Tanzania Coleman <tanzania.coleman@gmail.com>
2019-12-10 13:40:57 -0500

    added layout of page and updated server.js

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
 const express = require('express')
+const exphbs = require ('express-handlebars');
+const routes = require ('./controllers/burgers_controller');
+
+
+let app = express();
+let PORT = process.env.PORT || 8080;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+  console.log("Server listening on: http://localhost:" + PORT);
+});
