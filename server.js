109b917219b25f0f09a2c888152a354c08c8c5b6
Tanzania Coleman <tanzania.coleman@gmail.com>
2019-12-10 13:45:01 -0500

    removed body parser as it is not used

--- server.js
+++ server.js
@@ -7,8 +7,6 @@ let app = express();
 let PORT = process.env.PORT || 8080;
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
