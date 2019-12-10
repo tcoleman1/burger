72b267d23b252d2193396d8ce1cd72605b112ba4
Tanzania Coleman <tanzania.coleman@gmail.com>
2019-12-10 12:19:23 -0500

    updated orm functions and started on index.handlebars

--- config/orm.js
+++ config/orm.js
@@ -1,26 +1,38 @@
 let connection = require('./connection')
 
-//create selectAll(), insertOne() and updateOne() methods
 
 let orm = {
 
     selectAll: function(){
         
+        let allburgers = [];
          let queryString = "SELECT * FROM burgers"
          connection.query(queryString, function(err,res){
              if(err) throw err;
-            //  for(var i=0; i < res.length; i++){
-            //     if(res[i].devoured === "true"){
-
-            //     }
-            //  }
+             for(var i=0; i < res.length; i++){
+                if(res[i].devoured === "true"){
+                    allburgers.push(res[i])
+                }
+             }
          })
+
+         return allburgers;
+    },
+
+    insertOne: function(burger_name) {
+        let queryString = "INSERT INTO burgers(burger_name) VALUES (?)"
+        connection.query(queryString, [burger_name], function(err){
+            if (err) throw err;
+        })
+    },
+
+
+    updateOne: function (select){
+        let queryString = "UPDATE burgers SET devoured = true WHERE id =?"
+        connection.query(queryString, [select], function (err){
+            if(err) throw err;
+        })
     }
-
-    insertOne: 
-
-
-    updateOne
 
 }
 
